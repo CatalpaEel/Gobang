22bd159cda470465847af04fb9509be9ded9f29f
CatalpaEel <3245145219@qq.com>
2024-11-26 15:12:36 +0800

    Debug the checkban and add minmax

--- game.cpp
+++ game.cpp
@@ -54,8 +54,7 @@ public:
         int color = board[x][y];
         while (is_in_board(x, y) && board[x][y] == color)
         {
-            x += fx[dir][0];
-            y += fx[dir][1];
+            find_next(x, y, dir);
         }
         *resx = x;
         *resy = y;
@@ -64,47 +63,24 @@ public:
     // Input (x,y) which is a blank, then it will return the number of color in the direction of dir.
     int find_jump(int x, int y, int color, Direction dir)
     {
-        const int fx[8][2] = {{0, -1}, {0, 1}, {-1, 0}, {1, 0}, {-1, -1}, {1, -1}, {-1, 1}, {1, 1}};
-        x += fx[dir][0];
-        y += fx[dir][1];
+        find_next(x, y, dir);
         int cnt = 0;
         while (is_in_board(x, y) && board[x][y] == color)
         {
             ++cnt;
-            x += fx[dir][0];
-            y += fx[dir][1];
+            find_next(x, y, dir);
         }
         return cnt;
     }
 
-    int cal_row(int x, int y)
+    // change (x,y) to the next position in the direction of dir
+    void find_next(int &x, int &y, Direction dir)
     {
-        int lx, ly, rx, ry;
-        find_adjacent(x, y, LEFT, &lx, &ly);
-        find_adjacent(x, y, RIGHT, &rx, &ry);
-        return ry - ly - 1;
+        const int fx[8][2] = {{0, -1}, {0, 1}, {-1, 0}, {1, 0}, {-1, -1}, {1, -1}, {-1, 1}, {1, 1}};
+        x += fx[dir][0];
+        y += fx[dir][1];
     }
-    int cal_col(int x, int y)
-    {
-        int lx, ly, rx, ry;
-        find_adjacent(x, y, UP, &lx, &ly);
-        find_adjacent(x, y, DOWN, &rx, &ry);
-        return rx - lx - 1;
-    }
-    int cal_diag(int x, int y)
-    {
-        int lx, ly, rx, ry;
-        find_adjacent(x, y, LEFT_UP, &lx, &ly);
-        find_adjacent(x, y, RIGHT_DOWN, &rx, &ry);
-        return ry - ly - 1;
-    }
-    int cal_back_diag(int x, int y)
-    {
-        int lx, ly, rx, ry;
-        find_adjacent(x, y, LEFT_DOWN, &lx, &ly);
-        find_adjacent(x, y, RIGHT_UP, &rx, &ry);
-        return ry - ly - 1;
-    }
+
     bool checkwin()
     {
         const Direction fx[4][2] = {{LEFT, RIGHT}, {UP, DOWN}, {LEFT_UP, RIGHT_DOWN}, {LEFT_DOWN, RIGHT_UP}};
@@ -139,7 +115,7 @@ public:
     // If it is banned, return true, which means that the black loses the game.
     bool checkban()
     {
-        const Direction fx[4] = {RIGHT, DOWN, RIGHT_DOWN, RIGHT_UP};//to avoid repetition
+        const Direction fx[4][2] = {{LEFT, RIGHT}, {UP, DOWN}, {LEFT_UP, RIGHT_DOWN}, {LEFT_DOWN, RIGHT_UP}};
         int cnt = 0;
         for (int i = 1; i <= n; ++i)
         {
@@ -151,22 +127,23 @@ public:
                 }
                 for (int k = 0; k < 4; ++k)
                 {
-                    int x, y;
-                    find_adjacent(i, j, fx[k], &x, &y);
-                    int len = y - j - 1;
-                    if (i == 1)
+                    int lx = i, ly = j, rx, ry;
+                    find_next(lx, ly, fx[k][0]); // to avoid repetition
+                    find_adjacent(i, j, fx[k][1], &rx, &ry);
+                    int len = ry - ly - 1;
+                    if (k == 1)
                     {
-                        len = x - i - 1;
+                        len = rx - lx - 1;
                     }
                     if (len >= 6)
                     {
                         return true;
                     }
-                    else if (len == 4 && (canput(i-1, j-1) || canput(x, y)))
+                    else if (len == 4 && (canput(lx, ly) || canput(rx, ry)))
                     {
                         ++cnt;
                     }
-                    else if (len == 3 && canput(i-1, j-1) && canput(x, y))
+                    else if (len == 3 && canput(lx, ly) && canput(rx, ry))
                     {
                         ++cnt;
                     }
@@ -201,243 +178,230 @@ public:
     }
 };
 
-class NODE
+int evaluate_element(BOARD *board, int x, int y)
 {
-    BOARD *board;
-    NODE *head;
-    NODE *nxt;
-    int val = 0;
 
-    struct UPDATE
+    const Direction fx[4][2] = {{LEFT, RIGHT}, {UP, DOWN}, {LEFT_UP, RIGHT_DOWN}, {LEFT_DOWN, RIGHT_UP}};
+
+    int color = board->get_color(x, y);
+    if (color == -1)
     {
-        int x, y, color;
+        return 0;
+    }
+
+    const int INF = 1e7;
+    const int val_sheet[9] = {
+        20000,
+        8000,
+        8000,
+        6000,
+        2000,
+        2000,
+        1000,
+        500,
+        500,
     };
-    UPDATE update_stack[10];
-    int top_update_stack = 0;
-    void apply_update()
+
+    int cnt_open_four = 0;
+    int cnt_dead_four = 0;
+    int cnt_jump_four = 0;
+    int cnt_open_three = 0;
+    int cnt_sleep_three = 0;
+    int cnt_jump_three = 0;
+    int cnt_open_two = 0;
+    int cnt_sleep_two = 0;
+    int cnt_jump_two = 0;
+
+    for (int i = 0; i < 4; ++i)
     {
-        for (int i = 1; i <= top_update_stack; ++i)
+        int lx, ly, rx, ry;
+        board->find_adjacent(x, y, (Direction)fx[i][0], &lx, &ly);
+        board->find_adjacent(x, y, (Direction)fx[i][1], &rx, &ry);
+        int cnt_canput;
+        int len = ry - ly - 1;
+        if (i == 1)
         {
-            UPDATE update = update_stack[i];
-            board->board_put(update.x, update.y, update.color);
+            len = rx - lx - 1;
         }
-    }
-    void remove_update()
-    {
-        for (int i = 1; i <= top_update_stack; ++i)
+        switch (len)
         {
-            UPDATE update = update_stack[i];
-            board->board_put(update.x, update.y, -1);
-        }
-    }
-
-    int evaluate_element(int x, int y)
-    {
-
-        const Direction fx[4][2] = {{LEFT, RIGHT}, {UP, DOWN}, {LEFT_UP, RIGHT_DOWN}, {LEFT_DOWN, RIGHT_UP}};
-
-        int color = board->get_color(x, y);
-        if (color == -1)
-        {
-            return 0;
-        }
-
-        const int INF = 1e7;
-        const int val_sheet[9] = {
-            20000,
-            8000,
-            8000,
-            6000,
-            2000,
-            2000,
-            1000,
-            500,
-            500,
-        };
-
-        int cnt_open_four = 0;
-        int cnt_dead_four = 0;
-        int cnt_jump_four = 0;
-        int cnt_open_three = 0;
-        int cnt_sleep_three = 0;
-        int cnt_jump_three = 0;
-        int cnt_open_two = 0;
-        int cnt_sleep_two = 0;
-        int cnt_jump_two = 0;
-
-        for (int i = 0; i < 4; ++i)
-        {
-            int lx, ly, rx, ry;
-            board->find_adjacent(x, y, (Direction)fx[i][0], &lx, &ly);
-            board->find_adjacent(x, y, (Direction)fx[i][1], &rx, &ry);
-            int cnt_canput;
-            int len = ry - ly - 1;
-            if (i == 1)
+        case 5:
+            return color ? INF : -INF;
+            break;
+        case 4:
+            cnt_canput = board->canput(lx, ly) + board->canput(rx, ry);
+            switch (cnt_canput)
             {
-                len = rx - lx - 1;
-            }
-            switch (len)
-            {
-            case 5:
-                return color ? INF : -INF;
-                break;
-            case 4:
-                cnt_canput = board->canput(lx, ly) + board->canput(rx, ry);
-                switch (cnt_canput)
-                {
-                case 1:
-                    ++cnt_dead_four;
-                    break;
-                case 2:
-                    ++cnt_open_four;
-                    break;
-                }
-                break;
-            case 3:
-                cnt_canput = board->canput(lx, ly) + board->canput(rx, ry);
-                if (board->canput(lx, ly))
-                {
-                    if (board->find_jump(lx, ly, color, (Direction)fx[i][0]) == 1)
-                    {
-                        ++cnt_jump_four;
-                    }
-                    if (board->find_jump(rx, ry, color, (Direction)fx[i][1]) == 1)
-                    {
-                        ++cnt_jump_four;
-                    }
-                }
-                if (cnt_canput == 2)
-                {
-                    ++cnt_open_three;
-                }
-                else if (cnt_canput == 1)
-                {
-                    ++cnt_sleep_three;
-                }
+            case 1:
+                ++cnt_dead_four;
                 break;
             case 2:
-                cnt_canput = board->canput(lx, ly) + board->canput(rx, ry);
-                if (board->canput(lx, ly))
-                {
-                    switch (board->find_jump(lx, ly, color, (Direction)fx[i][0]))
-                    {
-                    case 1:
-                        ++cnt_jump_three;
-                        break;
-                    case 2:
-                        ++cnt_jump_four;
-                        break;
-                    }
-                    switch (board->find_jump(rx, ry, color, (Direction)fx[i][1]))
-                    {
-                    case 1:
-                        ++cnt_jump_three;
-                        break;
-                    case 2:
-                        ++cnt_jump_four;
-                        break;
-                    }
-                }
-                if (cnt_canput == 2)
-                {
-                    ++cnt_open_two;
-                }
-                else if (cnt_canput == 1)
-                {
-                    ++cnt_sleep_two;
-                }
-                break;
-            case 1:
-                if (board->canput(lx, ly))
-                {
-                    switch (board->find_jump(lx, ly, color, (Direction)fx[i][0]))
-                    {
-                    case 1:
-                        ++cnt_jump_two;
-                        break;
-                    case 2:
-                        ++cnt_jump_three;
-                        break;
-                    case 3:
-                        ++cnt_jump_four;
-                        break;
-                    }
-                    switch (board->find_jump(rx, ry, color, (Direction)fx[i][1]))
-                    {
-                    case 1:
-                        ++cnt_jump_two;
-                        break;
-                    case 2:
-                        ++cnt_jump_three;
-                        break;
-                    case 3:
-                        ++cnt_jump_four;
-                        break;
-                    }
-                }
-                break;
-            default: // >=6
-                return -INF;
+                ++cnt_open_four;
                 break;
             }
-
-            if (color && cnt_open_four + cnt_dead_four + cnt_open_three >= 2)
+            break;
+        case 3:
+            cnt_canput = board->canput(lx, ly) + board->canput(rx, ry);
+            if (board->canput(lx, ly))
             {
-                return -INF;
+                if (board->find_jump(lx, ly, color, (Direction)fx[i][0]) == 1)
+                {
+                    ++cnt_jump_four;
+                }
+                if (board->find_jump(rx, ry, color, (Direction)fx[i][1]) == 1)
+                {
+                    ++cnt_jump_four;
+                }
             }
+            if (cnt_canput == 2)
+            {
+                ++cnt_open_three;
+            }
+            else if (cnt_canput == 1)
+            {
+                ++cnt_sleep_three;
+            }
+            break;
+        case 2:
+            cnt_canput = board->canput(lx, ly) + board->canput(rx, ry);
+            if (board->canput(lx, ly))
+            {
+                switch (board->find_jump(lx, ly, color, (Direction)fx[i][0]))
+                {
+                case 1:
+                    ++cnt_jump_three;
+                    break;
+                case 2:
+                    ++cnt_jump_four;
+                    break;
+                }
+                switch (board->find_jump(rx, ry, color, (Direction)fx[i][1]))
+                {
+                case 1:
+                    ++cnt_jump_three;
+                    break;
+                case 2:
+                    ++cnt_jump_four;
+                    break;
+                }
+            }
+            if (cnt_canput == 2)
+            {
+                ++cnt_open_two;
+            }
+            else if (cnt_canput == 1)
+            {
+                ++cnt_sleep_two;
+            }
+            break;
+        case 1:
+            if (board->canput(lx, ly))
+            {
+                switch (board->find_jump(lx, ly, color, (Direction)fx[i][0]))
+                {
+                case 1:
+                    ++cnt_jump_two;
+                    break;
+                case 2:
+                    ++cnt_jump_three;
+                    break;
+                case 3:
+                    ++cnt_jump_four;
+                    break;
+                }
+                switch (board->find_jump(rx, ry, color, (Direction)fx[i][1]))
+                {
+                case 1:
+                    ++cnt_jump_two;
+                    break;
+                case 2:
+                    ++cnt_jump_three;
+                    break;
+                case 3:
+                    ++cnt_jump_four;
+                    break;
+                }
+            }
+            break;
+        default: // >=6
+            return -INF;
+            break;
         }
-        int sum = 0;
-        sum =
-            cnt_open_four * val_sheet[0] +
-            cnt_dead_four * val_sheet[1] +
-            cnt_jump_four * val_sheet[2] +
-            cnt_open_three * val_sheet[3] +
-            cnt_sleep_three * val_sheet[4] +
-            cnt_jump_three * val_sheet[5] +
-            cnt_open_two * val_sheet[6] +
-            cnt_sleep_two * val_sheet[7] +
-            cnt_jump_two * val_sheet[8];
-        return color ? sum : -sum;
-    }
 
-public:
-    NODE(BOARD *board)
-    {
-        this->board = board;
-    }
-    void add_update(int x, int y, int color)
-    {
-        update_stack[++top_update_stack] = (UPDATE){x, y, color};
-    }
-    void board_put(int x, int y, int color)
-    {
-        board->board_put(x, y, color);
-    }
-    int evaluate()
-    {
-        int sum = 0;
-        apply_update();
-        for (int i = 1; i <= n; ++i)
+        if (color && cnt_open_four + cnt_dead_four + cnt_open_three >= 2)
         {
-            for (int j = 1; j <= n; ++j)
+            return -INF;
+        }
+    }
+    int sum = 0;
+    sum =
+        cnt_open_four * val_sheet[0] +
+        cnt_dead_four * val_sheet[1] +
+        cnt_jump_four * val_sheet[2] +
+        cnt_open_three * val_sheet[3] +
+        cnt_sleep_three * val_sheet[4] +
+        cnt_jump_three * val_sheet[5] +
+        cnt_open_two * val_sheet[6] +
+        cnt_sleep_two * val_sheet[7] +
+        cnt_jump_two * val_sheet[8];
+    return color ? sum : -sum;
+}
+
+int evaluate(BOARD *board)
+{
+    int sum = 0;
+    if (board->checkban())
+    {
+        return -1e7;
+    }
+    for (int i = 1; i <= n; ++i)
+    {
+        for (int j = 1; j <= n; ++j)
+        {
+            sum += evaluate_element(board, i, j);
+            if (sum <= -1e7 || sum >= 1e7)
             {
-                sum += evaluate_element(i, j);
-                if (sum <= -1e7 || sum >= 1e7)
-                {
-                    remove_update();
-                    return sum;
-                }
+                return sum;
             }
         }
-        remove_update();
-        return sum;
     }
-    void add_child(NODE *child)
+    return sum;
+}
+
+int minmax(BOARD *board, int dep, int color, int &x, int &y)
+{
+    const int MAX_DEP = 3;
+    const int INF = 1e8;
+    if (dep == MAX_DEP)
     {
-        child->nxt = this->head;
-        this->head = child;
+        return evaluate(board);
     }
-};
+    int mx = color ? -INF : INF;
+    x = 0, y = 0;
+    for (int i = 1; i <= n; ++i)
+    {
+        for (int j = 1; j <= n; ++j)
+        {
+            if (board->canput(i, j))
+            {
+                if (!x)
+                {
+                    x = i, y = j;
+                }
+                board->board_put(i, j, color);
+                int tx, ty;
+                int val = minmax(board, dep + 1, color ^ 1, tx, ty);
+                if (color && val > mx || !color && val < mx)
+                {
+                    mx = val;
+                    x = i, y = j;
+                }
+                board->board_put(i, j, -1);
+            }
+        }
+    }
+    return mx;
+}
 
 bool player_go(BOARD *board, int color)
 {
@@ -457,38 +421,13 @@ bool player_go(BOARD *board, int color)
 
 bool computer_go(BOARD *board, int color)
 {
-    int x = 0, y = 0;
-    int mx = -1e8;
-    for (int i = 1; i <= n; ++i)
-    {
-        for (int j = 1; j <= n; ++j)
-        {
-
-            if (board->canput(i, j))
-            {
-                NODE *new_board = new NODE(board);
-                new_board->add_update(i, j, color);
-                int val = new_board->evaluate();
-                if (!color)
-                {
-                    val = -val;
-                }
-                if (val > mx)
-                {
-                    mx = val;
-                    x = i;
-                    y = j;
-                }
-                if (!x)
-                {
-                    x = i;
-                    y = j;
-                }
-                delete new_board;
-            }
-        }
-    }
+    int x, y;
+    minmax(board, 1, color, x, y);
     board->board_put(x, y, color);
+    if (color && board->checkban())
+    {
+        return false;
+    }
     return true;
 }
 bool go(BOARD *board, int opt, int color)
@@ -505,7 +444,6 @@ bool go(BOARD *board, int opt, int color)
 void play()
 {
     BOARD *board = new BOARD();
-    NODE *node = new NODE(board);
     board->printboard();
     int black = 1, white = black ^ 1;
     int cnt = 0;
